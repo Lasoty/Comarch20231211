2e20b68f9610a1d3413f8d31e7fa80d8e9e3da05
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2023-12-11 11:23:47 +0100

    Init commit

---
+++ BMICalculator/BMICalculator.Services/AutofixturePresentation/Example.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator.Services.AutofixturePresentation;
+
+public class Example
+{
+    private IDependency a;
+    private IDependency b;
+
+    public Example(IDependency one, IDependency two)
+    {
+        a = one ?? throw new ArgumentNullException(nameof(one));
+        b = two ?? throw new ArgumentNullException(nameof(two));
+    }
+}
+
+public class AnotherExpample
+{
+    private readonly IDependency one;
+    private readonly IDependency two;
+
+    public AnotherExpample(IDependency one, IDependency two)
+    {
+        this.one = one;
+        this.two = two;
+    }
+}
+
+public interface IDependency
+{
+}
+
+public class Dependency : IDependency
+{
+
+}
